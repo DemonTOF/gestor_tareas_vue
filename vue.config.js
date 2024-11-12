448c500dae59f0f5affe2d5d394448064ff08e36
DemonTOF <santi23py@gmail.com>
2024-11-12 02:08:04 -0300

    Correxion de direccion de repositorio

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'gestor_tareas_vue'
+    ? '/gestor_tareas_vue/'
     : '/'
 })
